76acf3ce7329c3503257ca39d92603fbc2fd952b
Qussai Khalil <qussaikhalil@icloud.com>
2023-02-13 21:36:57 +0100

    Startade projekt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HELLO MY FIRST GITHUB PROJECT ");
+        System.out.println("******************************");
     }
 }
